c092ad646213d548237be10b1acdc0bd3cd46b31
Rudá Porto Filgueiras <rudazz@gmail.com>
2016-08-31 13:22:20 +0200

    Create new content type: Form.

--- src/briefy/plone/tests/test_upgrades.py
+++ src/briefy/plone/tests/test_upgrades.py
@@ -23,7 +23,7 @@ class UpgradesTestCase(unittest.TestCase):
         """Test latest version of profile."""
         self.assertEqual(
             self.setup.getLastVersionForProfile(self.profile)[0],
-            u'20160825'
+            u'20160831'
         )
 
     def _match(self, item, source, dest):
@@ -31,7 +31,13 @@ class UpgradesTestCase(unittest.TestCase):
         return item['source'] == source and item['dest'] == dest
 
     def test_20160825_available(self):
-        """Test upgrade step is available."""
+        """Test upgrade step 20160825 is available."""
         steps = listUpgradeSteps(self.setup, self.profile, '20160816')
         steps = [s for s in steps if self._match(s[0], '20160816', '20160825')]
+        self.assertEqual(len(steps), 1)
+
+    def test_20160831_available(self):
+        """Test upgrade step 20160831 is available."""
+        steps = listUpgradeSteps(self.setup, self.profile, '20160825')
+        steps = [s for s in steps if self._match(s[0], '20160825', '20160831')]
         self.assertEqual(len(steps), 1)
