ef26babee832a947ad273deede36fb020bc4dafc
Érico Andrei <ericof@gmail.com>
2016-09-01 08:17:18 +0200

    Prepare release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 import os
 
-version = '0.1.0'
+version = '1.0.0'
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst')) as f:
