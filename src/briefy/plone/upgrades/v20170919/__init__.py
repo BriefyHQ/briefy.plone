660040d8b069bc7390fbea096ff028249933315a
Érico Andrei <ericof@gmail.com>
2017-09-19 19:50:12 +0200

    Remove blocks from search and sitemap

--- src/briefy/plone/upgrades/v20170919/__init__.py
+++ src/briefy/plone/upgrades/v20170919/__init__.py
@@ -15,3 +15,29 @@ def upgrade_plone_restapi(context):
     setup = api.portal.get_tool('portal_setup')
     setup.upgradeProfile(profile)
     logger.info('Upgrade plone_restapi')
+
+
+def remove_blocks_from_search(context):
+    """Remove blocks from search."""
+    record = 'plone.types_not_searched'
+    to_remove = (
+        'block_breezy',
+        'block_checker',
+        'block_columns',
+        'block_gallery',
+        'block_header',
+        'block_image',
+        'block_jumbotron',
+        'block_jumbotron_video',
+        'block_page',
+        'block_roster',
+        'col_block_item',
+        'Discussion Item',
+        'Image',
+        'Plone Site',
+        'row_block_checker',
+        'team_member',
+        'TempFolder'
+    )
+    api.portal.set_registry_record(record, to_remove)
+    logger.info('Blocks removed')
